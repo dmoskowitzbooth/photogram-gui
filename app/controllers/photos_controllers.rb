7a1a6a5c12e352035b3708f2f36e65cf1de00b15
dmoskowitzbooth <165198611+dmoskowitzbooth@users.noreply.github.com>
2024-05-08 02:05:04 +0000

    33

---
+++ app/controllers/photos_controllers.rb
@@ -0,0 +1,8 @@
+class PhotosController < ApplicationController
+
+  def index
+    pall=Photo.all
+    @list_of_photos = pall.order({ :created_at=> :desc })
+    render({ :template => "photos_templates/index"})
+end  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get("/users", { :controller => "users", :action => "homepage" })
   get("/users/:username", { :controller => "users", :action => "show" })
   post("/add_user", { :controller => "users", :action => "create" })
+
+  get("/photos", { :controller => "photos", :action => "index" })
 end
