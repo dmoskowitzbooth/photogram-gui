cad06d651b5ee987afa1259d7a139e68739b7fce
dmoskowitzbooth <165198611+dmoskowitzbooth@users.noreply.github.com>
2024-05-08 00:47:16 +0000

    2

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
 
   def homepage
+    matching_users=User.all
+    @list_of_users = matching_users.order({ :created_at => :desc })
     render({:template => "home"})
 
 end  
