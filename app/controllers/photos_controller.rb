b25637ade765c13a79cdebca9d01569e8d8cff35
dmoskowitzbooth <165198611+dmoskowitzbooth@users.noreply.github.com>
2024-05-09 18:29:14 +0000

    4

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -10,7 +10,11 @@ end
     the_photo=params.fetch("photo_id")
     matching_photo = Photo.where({ :id => the_photo })
     @the_photo = matching_photo.at(0)
-  
+
+    the_id=params.fetch("photo_id")
+    matching_comment = Comment.where({ :photo => the_id })
+    @the_comment = matching_photo.at(0)
+
     if the_photo==nil
         redirect_to("/")
     else
